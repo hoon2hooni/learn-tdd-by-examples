a4dc8d8adc9b4be5ef1f0d49ac5245a519e2e060
hoon2hooni <sanghoon.engineer@gmail.com>
2021-09-23 16:24:13 +0900

    chore: settings eslint and husky

---
+++ .eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  "parser": "@typescript-eslint/parser",
+  "plugins": ["@typescript-eslint"],
+  "extends": [
+    "airbnb-base",
+    "plugin:prettier/recommended",
+    "plugin:@typescript-eslint/recommended"
+  ]
+}
